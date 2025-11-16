19b6e03a4604076f447ca21a04d80434fab32a7f
noodle-strudel <yollaine369@gmail.com>
2025-11-15 18:22:42 -0800

    worked on camera stuff, door stuff

--- scenes/locations/outside.gd
+++ scenes/locations/outside.gd
@@ -1,25 +1,18 @@
 extends Node
 
-var boundary_left = -700
-var boundary_right = 700
-var boundary_top = 400
-var boundary_bottom = -400
-	
+@onready var boundary_left = $Base.boundary_left
+@onready var boundary_right = $Base.boundary_right
+@onready var boundary_top = $Base.boundary_top
+@onready var boundary_bottom = $Base.boundary_bottom
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_camera_limits()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	
 func set_camera_limits():
 	$Player/Camera2D.limit_left = boundary_left
 	$Player/Camera2D.limit_right = boundary_right
 	$Player/Camera2D.limit_top = boundary_top
 	$Player/Camera2D.limit_bottom = boundary_bottom
 
-func set_boundary():
+func _physics_process(delta: float) -> void:
 	pass
-	
