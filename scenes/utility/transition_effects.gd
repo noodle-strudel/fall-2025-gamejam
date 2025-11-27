c9c958a9e2e440ed8f8678c00a3dda6ec02ece7c
spencer1202 <76940991+spencer1202@users.noreply.github.com>
2025-11-27 01:41:12 -0800

    tidied up calling transition effects, fixed door transition

--- scenes/utility/transition_effects.gd
+++ scenes/utility/transition_effects.gd
@@ -1,16 +1,33 @@
-extends CanvasLayer
-
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func play_effect(effect: String) -> void:
+	match effect:
+		"player_fade_in":
+			$TransitionEffects/FullBlack/AnimationPlayer.play("fade_out")
+		"player_fade_out":
+			$TransitionEffects/FullBlack/AnimationPlayer.play_backwards("fade_out")
+		"background_fade_in":
+			$BackgroundBlack/AnimationPlayer.play_backwards("Fade")
+		"background_fade_out":
+			$TransitionEffects/FullBlack/AnimationPlayer.play_backwards("fade_out")
+		"vignette_fade_in":
+			$TransitionEffects/Vignette/AnimationPlayer.play_backwards("fade_out")
+		"vignette_fade_out":
+			$TransitionEffects/Vignette/AnimationPlayer.play("fade_out")
+		"blur":
+			$TransitionEffects/Blur/AnimationPlayer.play_backwards("fade_out")
+		"unblur":
+			$TransitionEffects/Blur/AnimationPlayer.play("fade_out")
 
-# Doesn't work
+
 func lift_all() -> void:
-	$Vignette/AnimationPlayer.play("fade_out")
-	$BlackButPlayer/AnimationPlayer.play("fade_outout")
-	$Blur/AnimationPlayer.play("fade_out")
+	$TransitionEffects/Vignette/AnimationPlayer.play("fade_out")
+	$TransitionEffects/FullBlack/AnimationPlayer.play("fade_outout")
+	$TransitionEffects/Blur/AnimationPlayer.play("fade_out")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
